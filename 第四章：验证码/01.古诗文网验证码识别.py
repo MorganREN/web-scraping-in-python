d0e061738eb04164e5249e8fcf3a51f7f3fcf22f
m33394mr <mohan.ren@student.manchester.ac.uk>
2021-07-19 18:41:30 +0800

    模拟登录古诗文网

--- 第四章：验证码/01.古诗文网验证码识别.py
+++ 第四章：验证码/01.古诗文网验证码识别.py
@@ -8,11 +8,11 @@ from lxml import html
 from chaojiying import Chaojiying_Client
 
 # 封装识别验证码的函数
-def getCodeText():
+def getCodeText(path, type):
     chaojiying = Chaojiying_Client('164689704', 'mohanyuehan123', '919739')  # 用户中心>>软件ID 生成一个替换 96001
-    im = open('code.jpg', 'rb').read()  # 本地图片文件路径 来替换 a.jpg 有时WIN系统须要//
+    im = open(path, 'rb').read()  # 本地图片文件路径 来替换 a.jpg 有时WIN系统须要//
     # print(chaojiying.PostPic(im, 1902))  # 1902 验证码类型  官方网站>>价格体系 3.4+版 print 后要加()
-    return chaojiying.PostPic(im, 1902)['pic_str']
+    return chaojiying.PostPic(im, type)['pic_str']
 
 
 if __name__ == "__main__":
@@ -31,4 +31,4 @@ if __name__ == "__main__":
         fp.write(img_data)
 
     # 调用超级鹰
-    print(getCodeText())
+    result = getCodeText("./code.jpg", 1902)
