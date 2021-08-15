c4f275149a200b821edce0f3c014ed4beca2c431
MorganREN <mohan.ren@student.manchester.ac.uk>
2021-08-15 22:22:28 +0800

    基本规避风险操作

--- 第七章：动态加载数据处理/06.谷歌无头浏览器+反检测.py
+++ 第七章：动态加载数据处理/06.谷歌无头浏览器+反检测.py
@@ -4,8 +4,10 @@ Author: Mohan Ren
 Date: 08//15//2021//
 """
 from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions # 实现规避检测
 from time import sleep
-from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.options import Options # 实现无头界面
 
 if __name__ == "__main__":
     # 无可视化界面（无头浏览器） phantomJs
@@ -13,7 +15,13 @@ if __name__ == "__main__":
     chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-gpu')
 
-    bro = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=chrome_options)
+
+    # 如何实现让selenium规避被检测到的风险
+    options = ChromeOptions()
+    options.add_experimental_option('excludeSwitches', ['enable-automation'])
+    driver = Chrome(options=options)
+
+    bro = webdriver.Chrome(executable_path='./chromedriver.exe', chrome_options=chrome_options, options=options)
     bro.get('https://www.baidu.com')
 
     print(bro.page_source)
